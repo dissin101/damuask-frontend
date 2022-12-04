832fa1ca91b3a5c39d5d938f10247572bea53090
kirillbushmanov <dissin101@gmail.com>
2022-12-04 13:25:41 +0600

    Split routes for auth and not auth user

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,24 +1,60 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  createRoutesFromElements, Navigate,
   Route,
   RouterProvider,
 } from 'react-router-dom'
 import Index from './index/Index'
 import Auth from './auth/Auth'
 import Patients from './patients/Patients'
+import { useSelector } from 'react-redux'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Index/>}/>
-      <Route path="/auth" element={<Auth/>}/>
-      <Route path="/patients" element={<Patients/>}/>
-    </>
+const Routes = () => {
+  
+  const isAuth = Boolean(useSelector((state) => state.auth.user))
+  
+  let router;
+  
+  if (isAuth){
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <>
+          <Route path="/" element={<Index/>}/>
+          <Route path="/patients" element={<Patients/>}/>
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
+        </>
+      )
+    )
+  } else {
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <>
+          <Route path="/auth" element={<Auth/>}/>
+          <Route
+            path="*"
+            element={<Navigate to="/auth" replace />}
+          />
+        </>
+      )
+    )
+  }
+  
+  /*const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Index/>}/>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/patients" element={<Patients/>}/>
+      </>
+    )
+  )*/
+  
+  return(
+    <RouterProvider router={router}/>
   )
-)
-
-const Routes = () =>
-  <RouterProvider router={router}/>
+}
 
 export default Routes
